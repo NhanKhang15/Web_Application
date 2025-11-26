569eae56d4747519cb103ee7f6c213a9bd2bd902
Andy-v-04 <namv0482@gmail.com>
2025-11-26 17:10:26 +0700

    feat(merchant): add Wallet Section + StatCard and modify MerchantProfile can render wallet

---
+++ frontend/src/components/app_panel/user_infor/screens/wallet/StatCard.jsx
@@ -0,0 +1,31 @@
+// frontend/src/components/dashboard/user_infor/screens/wallet/StatCard.jsx
+import React from 'react';
+
+/**
+ * Component tái sử dụng cho một mục chỉ số
+ * @param {Object} props
+ * @param {React.ReactNode} props.icon - Biểu tượng hiển thị
+ * @param {string} props.label - Nhãn của mục chỉ số
+ * @param {string} props.value - Giá trị của mục chỉ số
+ */
+export default function StatCard({ icon, label, value }) {
+    return (
+        <div className="flex items-center space-x-4">
+            {/* Vùng chứa Icon */}
+            <div className="flex-shrink-0 text-4xl">
+                {icon}
+            </div>
+        
+            {/* Vùng chứa Text*/}
+            <div>
+                <p className="text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                    {label}
+                </p>
+                <p className="text-2xl font-bold text-gray-900 mt-1">
+                    {value}
+                </p>
+            </div>
+        </div>
+
+    )
+}
